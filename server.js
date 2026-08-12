86b7a562b66411ddbc19a16ae82071481781217a
shifinozo <shifinshifu246@gmail.com>
2026-08-12 17:52:56 +0530

    feat: update server configuration and middleware settings in server.js

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ app.use(
       process.env.CLIENT_URL || "http://localhost:5173",
       process.env.FRONTEND_URL || "http://localhost:3000",
       "https://chameri-admin-dashboard.vercel.app",
+      "https://cb-admin.chameribuilders.com",
       "http://localhost:5173",
       "http://localhost:5174",
       "http://localhost:3000",
